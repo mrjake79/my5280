fa79091f7139e85c6052c893d91be60a476437a3
Jake Bahnsen <mrjake79@gmail.com>
2015-06-11 20:33:34 -0600

    Allow preview of handicaps to update before making changes.

--- admin/update_handicaps.php
+++ admin/update_handicaps.php
@@ -1,27 +1,40 @@
-<p>The following changes were made to player handicaps:</p>
-<table>
-    <thead>
-        <tr>
-            <th>Player</th>
-            <th>Previous Games</th>
-            <th>Previous Points</th>
-            <th>Previous Handicap</th>
-            <th>New Games</th>
-            <th>New Points</th>
-            <th>New Handicap</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($changed as $p): ?>
-            <tr>
-                <td><?php print $p['name']; ?></td>
-                <td><?php print $p['currentGames']; ?></td>
-                <td><?php print $p['currentPoints']; ?></td>
-                <td><?php print $p['currentHandicap']; ?></td>
-                <td><?php print $p['actualGames']; ?></td>
-                <td><?php print $p['actualPoints']; ?></td>
-                <td><?php print $p['actualHandicap']; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<form method="post">
+    <?php if(count($changed) == 0): ?>
+        <p>There are no corrections needed.</p>
+    <?php else: ?>
+        <?php if(!$saveChanges): ?>
+            <p>The following corrections are needed for handicaps:</p>
+        <?php else: ?>
+            <p>The changes were made successfully:</p>
+        <?php endif; ?>
+        <table class="widefat">
+            <thead>
+                <tr>
+                    <th scope="col">Player</th>
+                    <th scope="col">Previous Games</th>
+                    <th scope="col">Previous Points</th>
+                    <th scope="col">Previous Handicap</th>
+                    <th scope="col">New Games</th>
+                    <th scope="col">New Points</th>
+                    <th scope="col">New Handicap</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($changed as $p): ?>
+                    <tr>
+                        <td><?php print $p['name']; ?></td>
+                        <td><?php print $p['currentGames']; ?></td>
+                        <td><?php print $p['currentPoints']; ?></td>
+                        <td><?php print $p['currentHandicap']; ?></td>
+                        <td><?php print $p['actualGames']; ?></td>
+                        <td><?php print $p['actualPoints']; ?></td>
+                        <td><?php print $p['actualHandicap']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php if(!$saveChanges): ?>
+            <p class="submit"><input type="submit" name="action" value="Update &raquo;" class="button" /></p>
+        <?php endif; ?>
+    <?php endif; ?>
+</form>
